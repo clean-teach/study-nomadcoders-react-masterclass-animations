ba4297cd4ed6df0596c2eb67b584d3a33d043927
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-16 11:53:20 +0900

    #8.1 Installation
    # ****#8.0 Introduction****
    
    ## 개요
    
    - 이번 섹션에서는 Framer Motion을 써서 애니메이션을 만드는 법에 대해 학습한다.
    
    ## Framer Motion
    
    [Production-Ready Animation Library for React | Framer Motion](https://www.framer.com/motion/)
    
    - `ReactJS` 용 라이브러리
    - 애니메이션을 쉽게 만들 수 있다.
    - Pose 라는 라이브러리에서 시작됐다.
    - [Framer](https://www.framer.com/)는 디자이너를 위해 프로토타입을 만들어주는 회사이다.
        - Framer Motion 라이브러리는 Framer에서 만들었다.
    
    # ****#8.1 Installation****
    
    ## 목표
    
    - 프로젝트에 Framer Motion 을 설치해본다.
    - Framer Motion 을 사용해서 애니메이션을 만들어 본다.
    
    ## 사용법
    
    [Introduction | Framer for Developers](https://www.framer.com/docs/introduction/#quick-start)
    
    1. `npm install framer-motion` 설치
        - create-react-app Version 4 이하에서는 에러가 발생 할 것이다.
    
            → [Ecma Script Module 을 허용해야 한다.](https://www.notion.so/08-ANIMATIONS-d7c84838163c42e9980adba335f04f14)
    
            - create-react-app Version 5에서는 Ecma Script Module를 허용하기 때문에 해당 에러가 발생하지 않는다.
    2. `import { motion } from "framer-motion"` 불러오기
    3. `[<motion.div />` 컴포넌트](https://www.framer.com/docs/introduction/##the-motion-component)
        - Framer Motion 을 사용하기 위해서 일반 HTML Element를 사용 할 수는 없다.
    
            → Framer Motion이 제공하는 `motion` 패키지를 통해서 컴포넌트를 생성 한다.
    
    ### `[craco](https://github.com/dilanx/craco)`
    
    - create-react-app 의 설정사항을 **O**verride(무시) 해주는 라이브러리
    - 사용법
        1. `npm i -D @craco/craco` 설치
        2. `craco.config.js` 파일 생성
            - `package.json` 과 같은 단계에 위치
        3. 해당 파일 설정이 필요한 곳에서 제공하는 문서를 따라서 `craco.config.js` 작성
            - 예 : [https://github.com/framer/motion/issues/1307#issuecomment-955877279](https://github.com/framer/motion/issues/1307#issuecomment-955877279)
        4. `package.json` 의 script 명령어 수정
    
            ```json
            "scripts": {
            ~~-  "start": "react-scripts start"~~
            **+  "start": "craco start"**
            ~~-  "build": "react-scripts build"~~
            **+  "build": "craco build"**
            ~~-  "test": "react-scripts test"~~
            **+  "test": "craco test"**
            }
            ```

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Box = styled.div`
@@ -17,6 +21,7 @@ function App() {
   return (
     <Wrapper>
       <Box />
+      <motion.div></motion.div>
     </Wrapper>
   );
 }
