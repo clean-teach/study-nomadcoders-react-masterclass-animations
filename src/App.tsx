01582ae0090a750ed21ce6fb8e3245be785842c2
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-16 18:50:31 +0900

    #8.3 Variants part One 및 실습별로 컴포넌트 분리
    - 코드를 깔끔하게 해준다.
    - 많은 애니메이션을 하나로 연결 해준다.
        - 애니메이션의 Stage(무대)이다.
    - 애니메이션 설정을 오브젝트로 분리 할 수 있다.
    
    ## ****Variants 기본**** 사용법
    
    ```tsx
    const myVars = {
      start: { scale: 0 },
      end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
    };
    
    <motion.div variants={myVars} initial="start" animate="end" />
    ```
    
    1. Object로 만든 애니메이션을 `variants` Props에 전달
        - Object 이름과 그 안의 Key 이름은 마음대로 설정 할 수 있다.
    
            → `<motion>` 의 `Props` 랑만 올바르게 연결 하면 된다.
    
    2. Object 의 초기 스타일 부분 key name 을 `initial` Props에 전달
    3. Object 의 애니메이션 부분 key name 을 `animate` Props에 전달
        - `transition` 은 animate 안에 들어간다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import BasicAnimations from './components/BasicAnimations';
 
 const Wrapper = styled.div`
   display: flex;
@@ -9,22 +9,10 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: 'spring', delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <BasicAnimations />
     </Wrapper>
   );
 }
